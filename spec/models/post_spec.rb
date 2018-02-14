697ec46aac1875c194cc8944d5fb7149d4fd9122
lynnf1 <linnf@live.com>
2018-02-14 04:39:12 +0000

    Styled show page and added links to show and edit veiws

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,11 +11,16 @@ require 'rails_helper'
    		expect(@post).to be_valid
    	end
  
-   	it 'cannot be created without a date and rationale' do
+   	it 'cannot be created without a date and rationale, and overtime_request' do
    		@post.date = nil
    		@post.rationale = nil
      @post.overtime_request = nil
    		expect(@post).to_not be_valid
+   	end
+   	
+   	it 'has an overtime_request greater than 0.0' do
+   	 @post.overtime_request = 0.0
+   	 
    	end
    end
  end
