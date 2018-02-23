5bc03bc2e64e9c760c0d3885fa44d624e193142f
lynnf1 <linnf@live.com>
2018-02-23 21:24:16 +0000

    Brought in live data for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,8 @@ puts "1 User created"
 
 
 30.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+	Post.create!(date: Date.today, rationale: "#{post} Aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "30 Posts have been created"
