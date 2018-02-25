8cfa41e489d6a6e5cc0b3cb65491a6de0d287d7a
lynnf1 <linnf@live.com>
2018-02-25 00:06:12 +0000

    Added new view elements for audit items that are pending

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -7,7 +7,7 @@ describe 'homepage' do
     login_as(admin_user, :scope => :user)
     
     visit root_path
-    click_on('approve_#{post.id}')
+    click_on("approve_#{post.id}")
     expect(post.reload.status).to eq('approved')
   end
 end
